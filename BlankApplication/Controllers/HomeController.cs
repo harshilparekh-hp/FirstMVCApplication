720a64b2a9d8c896aeee7712c403cbcffe973eee
harshil.parekh <harshil.parekh@STALAP-1126>
2024-02-01 09:07:26 -0600

    Add project files.

---
+++ BlankApplication/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using BlankApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlankApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(int id)
+        {
+            EmployeeBusinessLogic employeeBusinessLogic = new EmployeeBusinessLogic();
+            Employee emp = employeeBusinessLogic.GetEmployee(id);
+
+            // we can pass the model data from controller to view using ViewData, ViewBag and TempData. 
+
+            // passing employee model to the view using ViewData
+
+            // Represents a container that is used to pass data between a controller and a view
+
+            ViewData["EmployeeData"] = emp; // passing employee object to ViewData
+            ViewData["Header"] = "Employee Details"; // passing string to ViewData
+            return View();
+        }
+
+    
+    }
+}
---
+++ BlankApplication/Models/EmployeeBusinessLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlankApplication.Models
+{
+    /// <summary>
+    /// Business logic to get the data of employee. This is usually to get the data from Database.
+    /// </summary>
+    public class EmployeeBusinessLogic
+    {
+        public Employee GetEmployee(int employeeId)
+        {
+            Employee employee = new Employee()
+            {
+                EmployeeId = employeeId,
+                Name = "Harshil",
+                Address = "Markham",
+                City = "Winnipeg",
+                Gender = "Male"
+            };
+
+            return employee;
+        }
+    }
+}
